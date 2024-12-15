648e6e160ddc3f3396eb7c3595e055765b73a597
mlereno <pedro.lereno@outlook.com>
2024-12-15 11:42:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function verificarFimDeJogo() {
 
 async function registrarUsuario(nick, password) {
     try {
-        const response = await fetch('http://localhost:3000/register', {
+        const response = await fetch('./register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ nick, password }),
